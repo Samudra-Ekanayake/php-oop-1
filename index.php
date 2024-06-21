c10a176b40499af4b2ad58845972f6a7e364bc5f
Samudra-Ekanayake <samudra2724@gmail.com>
2024-06-21 19:01:33 +0200

    Stampa del primo film

--- index.php
+++ index.php
@@ -1,5 +1,57 @@
 <?php
 
+class Movies {
+    public $name;
+    public $cost;
+    public $where;
+
+
+
+function setName($name) {
+    $this->name = $name;
+}
+
+function getName() {
+   return $this->name;
+}
+
+function setCost($cost) {
+    $this->cost = $cost;
+}
+
+function getCost() {
+    return $this->cost;
+}
+function setWhere($where) {
+    $this->where = $where;
+}
+
+function getWhere() {
+    return $this->where;
+}
+
+}
+
+
+$spiderman = new movies ();
+$spiderman->setName ('Spiderman');
+$spiderman->setCost('10.00€');
+$spiderman->setWhere('Milano');
+
+$nemo = new movies ();
+$nemo->setName ('Nemo');
+$nemo->setCost('8.00€');
+$nemo->setWhere('Firenze');
+
+$frozen = new movies ();
+$frozen->setName ('Frozen');
+$frozen->setCost('12.00€');
+$frozen->setWhere('Parma');
+
+$madagascar = new movies ();
+$madagascar->setName ('madagascar');
+$madagascar->setCost('5.00€');
+$madagascar->setWhere('Torino');
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +65,12 @@
 <body>
 
 <?php
+
+echo "Name: " . $spiderman->getName();
+echo "<br>";
+echo "Cost: " .  $spiderman->getCost();
+echo "<br>";
+echo "Where: " . $spiderman->getWhere();
 
 ?>
     
